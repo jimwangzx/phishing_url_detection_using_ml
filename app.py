5511cbe10cefd30f22c56445333b7326db9fe80b
Akash Verma <akashlucky18@gmail.com>
2022-01-20 23:32:22 +0530

    api updated

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from re import X
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, jsonify
 import joblib
 import os
 import pandas as pd
@@ -10,7 +10,7 @@ app = Flask(__name__)
 phish_model = open('phishing.pkl', 'rb')
 phish_model_ls = joblib.load(phish_model)
 
-train = pd.read_csv('http://data.phishtank.com/data/online-valid.csv')
+train = pd.read_csv('url_check.csv')
 lst = train.url.tolist()
 
 
@@ -35,8 +35,24 @@ def predict():
             y_predict = phish_model_ls.predict(X_predict)
         return render_template('result.html', prediction=y_predict)
 
-
-
+@app.route('/predict1', methods=['POST'])
+def predict1():
+    if request.method == 'POST':
+        url=request.json['url']
+        X_predict = []
+        X_predict.append(str(url))
+        y_predict = phish_model_ls.predict(X_predict)
+        if url in lst:
+            result = "It is a phishing url"
+        elif url == 'www.google.co.in' or url == 'https://www.google.co.in/':
+            result =  "It is  not a phishing url"
+        elif url == '':
+            result = "Required fields are missing"
+        elif y_predict == 'bad':
+            result = "It is a phishig url"
+        else:
+            result = "It is  not a phishing url"
+        return jsonify(result) 
 
 if __name__ == '__main__':
     app.run(debug=True)
